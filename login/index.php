5506389c60c55e791c049a65b62ff549102d2836
Yonathan Castillo <leothan522@gmail.com>
2024-12-20 11:56:24 -0400

    Nuevo Login

--- login/index.php
+++ login/index.php
@@ -5,11 +5,17 @@ use app\controller\GuestController;
 $controller = new GuestController();
 $controller->index();
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="es">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Yonathan Castillo and Bootstrap contributors">
+    <meta name="generator" content="leothan 0.1">
+
+    <title><?php echo APP_NAME ?> | Inicia sesión</title>
+
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="57x57" href="<?php asset('public\\favicon\\apple-icon-57x57.png') ?>">
     <link rel="apple-touch-icon" sizes="60x60" href="<?php asset('public\\favicon\\apple-icon-60x60.png') ?>">
@@ -29,105 +35,229 @@ $controller->index();
     <meta name="msapplication-TileImage" content="<?php asset('public\\favicon\\ms-icon-144x144.png') ?>">
     <meta name="theme-color" content="#ffffff">
 
-    <title><?php echo APP_NAME ?> | Inicia sesión</title>
+    <!--Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php asset('app/resources/adminlte/plugins/fontawesome-free/css/all.min.css'); ?>">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet"
-          href="<?php asset('app/resources/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css'); ?>">
-    <!-- SweetAlert2 -->
-    <link rel="stylesheet"
-          href="<?php asset('app/resources/adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css'); ?>">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="<?php asset('app/resources/adminlte/dist/css/adminlte.min.css'); ?>">
+    <style>
+        *{
+            font-family: "Poppins", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+        }
+
+        .text_title{
+            color: rgba(8,23,44,1);
+            font-weight: bold;
+        }
+
+
+        .gradient-custom-2 {
+            /* fallback for old browsers */
+            background: rgb(18,58,108);
+
+            /* Chrome 10-25, Safari 5.1-6 */
+            background: -webkit-radial-gradient(circle, rgba(18,58,108,1) 0%, rgba(8,23,44,1) 100%);
+
+            /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            background: radial-gradient(circle, rgba(18,58,108,1) 0%, rgba(8,23,44,1) 100%);
+        }
+
+        @media (min-width: 768px) {
+            .gradient-form {
+                height: 100vh !important;
+            }
+        }
+        @media (min-width: 769px) {
+            .gradient-custom-2 {
+                border-top-right-radius: .3rem;
+                border-bottom-right-radius: .3rem;
+            }
+        }
+
+
+        .gobernacion{
+            display: block;
+            position: absolute;
+            height: 80px;
+            width: 80px;
+            right: 3%;
+            top: 3%;
+        }
+
+        .gobernacion_start{
+            display: block;
+            position: absolute;
+            height: 100px;
+            width: 100px;
+            left: 3%;
+            top: 3%;
+        }
+
+
+    </style>
+
+
+
 </head>
-<body class="hold-transition login-page">
-<!-- Preloader -->
-<?php require_once "../admin/_layout/preloader.php" ?>
+<body>
 
-<div class="login-box">
-    <!-- /.login-logo -->
-    <div class="card card-outline card-primary">
-        <div class="card-header text-center p-0">
-            <a href="<?php echo APP_DOMINIO; ?>">
-                <img class="img-thumbnail border-transparent" src="<?php asset('public/img/logo_alguarisa.jpg'); ?>" alt="Alimentos del Guárico">
-            </a>
+<!-- Login 8 - Bootstrap Brain Component -->
+<section class="bg-light p-3 p-md-4 p-xl-5 position-relative" style="min-height: 100vh;">
+    <div class="container  position-absolute top-50 start-50 translate-middle">
+        <div class="row justify-content-center">
+            <div class="col-12 col-xxl-11">
+                <div class="card border-light-subtle shadow-sm">
+                    <div class="row g-0">
+
+                        <div class="col-12 col-md-6 d-none d-lg-flex gradient-custom-2">
+                            <img class="img-fluid rounded-start w-100 h-100 object-fit-fill" loading="lazy"
+                                 src="<?php asset('public/img/logo_tecnologia.png'); ?>" alt="Logo tecnologia">
+                        </div>
+
+
+                        <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                            <div class="col-12 col-lg-11 col-xl-10">
+                                <div class="card-body p-3 p-md-4 p-xl-5">
+
+                                    <img class="gobernacion_start d-sm-none" src="<?php asset('public/img/logo_gobernacion.svg'); ?>" alt="Logo Gobernacion">
+
+                                    <div class="row">
+                                        <div class="col-12 text-center">
+                                            <a href="<?php echo APP_DOMINIO; ?>">
+                                                <img class="img-fluid d-none d-lg-inline-flex w-50 mb-3" src="<?php asset('public/img/logo_gobernacion.svg'); ?>" alt="Logo Gobernacion">
+                                                <img class="img-fluid d-lg-none mt-5 mb-5" src="<?php asset('public/img/logo_alguarisa.png'); ?>" alt="Logo Alguarisa">
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                    <form class="position-relative" id="form_login"  novalidate>
+                                        <div class="row gy-3 overflow-hidden">
+                                            <div class="col-12">
+                                                <div class="form-floating mb-2 has-validation">
+                                                    <input id="email" type="email" class="form-control" name="email"  placeholder="name@example.com" required>
+                                                    <label for="email" class="form-label">Correo electrónico</label>
+                                                    <div class="invalid-feedback" id="error_email">
+                                                        Por favor ingrese su Correo electrónico.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="form-floating mb-2 has-validation">
+                                                    <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
+                                                    <label for="password" class="form-label">Contraseña</label>
+                                                    <div class="invalid-feedback" id="error_password">
+                                                        Por favor ingrese su Contraseña.
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12">
+                                                <div class="d-grid">
+                                                    <input type="hidden" name="opcion" value="login">
+                                                    <button class="btn btn-dark btn-lg gradient-custom-2" type="submit">
+                                                        Iniciar sesión
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="position-absolute top-50 start-50 translate-middle d-none verCargando">
+                                            <div class="spinner-border" role="status">
+                                                <span class="visually-hidden">Loading...</span>
+                                            </div>
+                                        </div>
+                                    </form>
+
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-center mt-5">
+                                                <?php if (env('APP_REGISTER', false)){ ?>
+                                                    <a href="../register" class="link-secondary text-decoration-none">
+                                                        Registrarse
+                                                    </a>
+                                                <?php } ?>
+                                                <a href="../forgot-password" class="link-secondary text-decoration-none">
+                                                    ¿Olvidó su contraseña?
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-12 text-center mt-5">
+                                            <small class="link-secondary text-decoration-none">
+                                                &copy; 2024 Dirección de Tecnología y Sistemas.
+                                            </small>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="card-body" id="card_form_login">
-            <p class="login-box-msg">Inicia sesión para ingresar</p>
-
-            <form id="form_login">
-                <div class="input-group mb-3">
-                    <input type="email" class="form-control" placeholder="Email" name="email" id="email">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
-                    <div class="invalid-feedback" id="error_email"></div>
-                </div>
-                <div class="input-group mb-3">
-                    <input type="password" class="form-control" placeholder="Contraseña" name="password" id="password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                    <div class="invalid-feedback" id="error_password"></div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        <div class="icheck-primary">
-                            <input type="checkbox" id="remember">
-                            <label for="remember" class="text-xs">
-                                Mostrar Contraseña
-                            </label>
-                        </div>
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-6">
-                        <input type="hidden" name="opcion" value="login">
-                        <button type="submit" class="btn btn-primary btn-block">
-                            Iniciar sesión
-                        </button>
-                    </div>
-                    <!-- /.col -->
-                </div>
-            </form>
-
-            <p class="mb-1 mt-3">
-                <a href="../forgot-password">Olvidé mi contraseña</a>
-            </p>
-            <?php if (env('APP_REGISTER', false)){ ?>
-                <p class="mb-0">
-                    <a href="../register" class="text-center">Registrarse</a>
-                </p>
-            <?php } ?>
-        </div>
-        <!-- /.card-body -->
-
-        <?php verCargando(); ?>
-
     </div>
-    <!-- /.card -->
+</section>
+
+<!-- Toast Bootstrap con JS -->
+<div id="toastBootstrap">
+    <!-- JS -->
 </div>
-<!-- /.login-box -->
 
-<!-- jQuery -->
-<script src="<?php asset('app/resources/adminlte/plugins/jquery/jquery.min.js'); ?>"></script>
-<!-- Bootstrap 4 -->
-<script src="<?php asset('app/resources/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-<!-- AdminLTE App -->
-<script src="<?php asset('app/resources/adminlte/dist/js/adminlte.min.js'); ?>"></script>
-<!-- SweetAlert2 -->
-<script src="<?php asset('app/resources/adminlte/plugins/sweetalert2/sweetalert2.min.js'); ?>"></script>
-<script src="<?php asset('public/js/sweetalert-app.js', true); ?>"></script>
-<script src="<?php asset('public/js/app.js', true); ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="<?php asset('public/js/toastBootstrap.js', true); ?>"></script>
+<script src="<?php asset('login/_app/app.js', true); ?>"></script>
+<script type="application/javascript">
 
-<script src="<?php asset('login/_app/login.js', true); ?>"></script>
+    const form = document.querySelector('#form_login');
+    const email = document.querySelector('#email');
+    const password = document.querySelector('#password');
+    const error_email = document.querySelector('#error_email');
+    const error_password = document.querySelector('#error_password');
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        event.stopPropagation();
+        form.classList.add('was-validated');
+        if (form.checkValidity()) {
+            verCargando('form_login');
+            const url = "_request/LoginRequest.php";
+            ajaxRequest({ url: url, form: form }, function (data) {
+                if (data.result){
+                    window.location.replace("../admin/");
+                }else {
+                    form.classList.remove('was-validated');
+                    email.classList.remove('is-invalid');
+                    password.classList.remove('is-invalid');
+
+                    if (data.error === "no_email"){
+                        email.classList.add('is-invalid');
+                        error_email.textContent = data.message;
+                    }
+
+                    if (data.error === "no_password"){
+                        password.classList.add('is-invalid');
+                        error_password.textContent = data.message;
+                    }
+
+                    if (data.error === "no_activo"){
+                        email.classList.add('is-invalid');
+                        error_email.textContent = data.message;
+                    }
+
+                    verCargando('form_login', false);
+                }
+            });
+        }
+    });
+
+    console.log('Hi!');
+</script>
 </body>
 </html>
