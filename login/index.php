687515cb9c1595abfac94986aec58adedfe37f56
Yonathan Castillo <leothan522@gmail.com>
2025-01-25 13:38:11 -0400

    Preloader

--- login/index.php
+++ login/index.php
@@ -42,6 +42,11 @@ $controller->index();
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
 
     <style>
+		@media (min-width: 768px) {
+            #scale {
+                transform: scale(0.8); /* Reduce el tamaño al 80% */
+            }
+        }
         *{
             font-family: "Poppins", sans-serif;
             font-weight: 400;
@@ -98,16 +103,64 @@ $controller->index();
 
 
     </style>
+	
+	<style>
+    /* styles.css */
+    #preloader {
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background: #fff no-repeat center center;
+        z-index: 9999;
+    }
+
+    #preloader::before {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100px;
+        height: 100px;
+        background: url('<?php asset('public/img/logo_alguarisa.png'); ?>') no-repeat center center;
+        background-size: contain;
+        transform: translate(-50%, -50%);
+        animation: pulse 2s infinite;
+    }
+
+    @keyframes pulse {
+        0% {
+            transform: translate(-50%, -50%) scale(1);
+        }
+        50% {
+            transform: translate(-50%, -50%) scale(1.2);
+        }
+        100% {
+            transform: translate(-50%, -50%) scale(1);
+        }
+    }
+
+</style>
+<script type="application/javascript">
+    //Script para ejecurar el preloader
+    window.addEventListener('load', function() {
+        document.querySelector('#preloader').style.display = 'none';
+        document.querySelector('.container').style.display = 'block';
+    });
+</script>
 
 
 
 </head>
 <body>
 
+<div id="preloader"></div>
+
 <!-- Login 8 - Bootstrap Brain Component -->
 <section class="bg-light p-3 p-md-4 p-xl-5 position-relative" style="min-height: 100vh;">
     <div class="container  position-absolute top-50 start-50 translate-middle">
-        <div class="row justify-content-center">
+        <div id="scale" class="row justify-content-center">
             <div class="col-12 col-xxl-11">
                 <div class="card border-light-subtle shadow-sm">
                     <div class="row g-0">
