fcd21c9da50c07bd8e193afbb2c4fba66696b0e7
Yonathan Castillo <leothan522@gmail.com>
2025-07-02 08:17:11 -0400

    Nuevo Login

--- login/_index.php
+++ login/_index.php
@@ -11,7 +11,7 @@ $controller->index();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="Ing. Yonathan Castillo">
+    <meta name="author" content="Yonathan Castillo and Bootstrap contributors">
     <meta name="generator" content="leothan 0.1">
 
     <title><?php echo APP_NAME ?> | Inicia sesión</title>
@@ -37,19 +37,16 @@ $controller->index();
 
     <!--Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
 
     <style>
-
-        @media (min-width: 768px) {
+		@media (min-width: 768px) {
             #scale {
                 transform: scale(0.8); /* Reduce el tamaño al 80% */
             }
         }
-
         *{
             font-family: "Poppins", sans-serif;
             font-weight: 400;
@@ -89,8 +86,8 @@ $controller->index();
         .gobernacion{
             display: block;
             position: absolute;
-            height: 100px;
-            width: 100px;
+            height: 80px;
+            width: 80px;
             right: 3%;
             top: 3%;
         }
@@ -106,146 +103,160 @@ $controller->index();
 
 
     </style>
+	
+	<style>
+    /* styles.css */
+    #preloader {
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background: #fff no-repeat center center;
+        z-index: 9999;
+    }
 
-    <style>
-        /* styles.css */
-        #preloader {
-            position: fixed;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            background: #fff no-repeat center center;
-            z-index: 9999;
+    #preloader::before {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100px;
+        height: 100px;
+        background: url('<?php asset('public/img/logo_alguarisa.png'); ?>') no-repeat center center;
+        background-size: contain;
+        transform: translate(-50%, -50%);
+        animation: pulse 2s infinite;
+    }
+
+    @keyframes pulse {
+        0% {
+            transform: translate(-50%, -50%) scale(1);
         }
-
-        #preloader::before {
-            content: "";
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            width: 100px;
-            height: 100px;
-            background: url('{{ asset('img/logo_alguarisa.png') }}') no-repeat center center;
-            background-size: contain;
-            transform: translate(-50%, -50%);
-            animation: pulse 2s infinite;
+        50% {
+            transform: translate(-50%, -50%) scale(1.2);
         }
-
-        @keyframes pulse {
-            0% {
-                transform: translate(-50%, -50%) scale(1);
-            }
-            50% {
-                transform: translate(-50%, -50%) scale(1.2);
-            }
-            100% {
-                transform: translate(-50%, -50%) scale(1);
-            }
+        100% {
+            transform: translate(-50%, -50%) scale(1);
         }
+    }
 
-    </style>
-    <script type="application/javascript">
-        //Script para ejecurar el preloader
-        window.addEventListener('load', function() {
-            document.querySelector('#preloader').style.display = 'none';
-            document.querySelector('.container').style.display = 'block';
-        });
-    </script>
+</style>
+<script type="application/javascript">
+    //Script para ejecurar el preloader
+    window.addEventListener('load', function() {
+        document.querySelector('#preloader').style.display = 'none';
+        document.querySelector('.container').style.display = 'block';
+    });
+</script>
+
+
 
 </head>
 <body>
 
 <div id="preloader"></div>
 
-<div class="position-relative gradient-form" style="min-height: 100vh;">
-    <div class="position-absolute top-50 start-50 translate-middle container">
-
-
-        <div class="row d-flex justify-content-center align-items-center">
-            <div class="col-xl-10">
-                <div class="card rounded-3 text-black">
+<!-- Login 8 - Bootstrap Brain Component -->
+<section class="bg-light p-3 p-md-4 p-xl-5 position-relative" style="min-height: 100vh;">
+    <div class="container  position-absolute top-50 start-50 translate-middle">
+        <div id="scale" class="row justify-content-center">
+            <div class="col-12 col-xxl-11">
+                <div class="card border-light-subtle shadow-sm">
                     <div class="row g-0">
-                        <div class="col-lg-6">
-                            <div class="card-body p-md-5 mx-md-4">
 
-                                <img class="gobernacion_start d-lg-none" src="<?php asset('public/img/logo_gobernacion.png') ?>" alt="Logo Gobernación Guárico">
+                        <div class="col-12 col-md-6 d-none d-lg-flex gradient-custom-2">
+                            <img class="img-fluid rounded-start w-100 h-100 object-fit-fill" loading="lazy"
+                                 src="<?php asset('public/img/logo_tecnologia.png'); ?>" alt="Logo tecnologia">
+                        </div>
 
-                                <div class="text-center">
-                                    <a href="<?php echo APP_DOMINIO; ?>">
-                                        <img class="img-fluid mt-5" src="<?php asset('public/img/logo_alguarisa.png') ?>" alt="Logo Alguarisa">
-                                    </a>
-                                    <h6 class="mt-1 mb-4 pb-1 text_title"><strong>Dirección de Tecnología y Sistemas.</strong></h6>
-                                </div>
 
-                                <form class="position-relative" id="form_login"  novalidate>
-                                    <div class="row gy-3 overflow-hidden">
-                                        <div class="col-12">
-                                            <div class="form-floating mb-2 has-validation">
-                                                <input id="email" type="email" class="form-control" name="email"  placeholder="name@example.com" required>
-                                                <label for="email" class="form-label">Correo electrónico</label>
-                                                <div class="invalid-feedback" id="error_email">
-                                                    Por favor ingrese su Correo electrónico.
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <div class="form-floating mb-2 has-validation">
-                                                <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
-                                                <label for="password" class="form-label">Contraseña</label>
-                                                <div class="invalid-feedback" id="error_password">
-                                                    Por favor ingrese su Contraseña.
-                                                </div>
-                                            </div>
-                                        </div>
+                        <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                            <div class="col-12 col-lg-11 col-xl-10">
+                                <div class="card-body p-3 p-md-4 p-xl-5">
 
-                                        <div class="col-12">
-                                            <div class="d-grid">
-                                                <input type="hidden" name="opcion" value="login">
-                                                <button class="btn btn-dark btn-lg gradient-custom-2" type="submit">
-                                                    Iniciar sesión
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="position-absolute top-50 start-50 translate-middle d-none verCargando">
-                                        <div class="spinner-border" role="status">
-                                            <span class="visually-hidden">Loading...</span>
-                                        </div>
-                                    </div>
-                                </form>
+                                    <img class="gobernacion_start d-sm-none" src="<?php asset('public/img/logo_gobernacion.svg'); ?>" alt="Logo Gobernacion">
 
-                                <div class="row">
-                                    <div class="col-12">
-                                        <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-center mt-5">
-                                            <?php if (env('APP_REGISTER', false)){ ?>
-                                                <a href="../register" class="link-secondary text-decoration-none">
-                                                    Registrarse
-                                                </a>
-                                            <?php } ?>
-                                            <a href="../forgot-password" class="link-secondary text-decoration-none">
-                                                ¿Olvidó su contraseña?
+                                    <div class="row">
+                                        <div class="col-12 text-center">
+                                            <a href="<?php echo APP_DOMINIO; ?>">
+                                                <img class="img-fluid d-none d-lg-inline-flex w-50 mb-3" src="<?php asset('public/img/logo_gobernacion.svg'); ?>" alt="Logo Gobernacion">
+                                                <img class="img-fluid d-lg-none mt-5 mb-5" src="<?php asset('public/img/logo_alguarisa.png'); ?>" alt="Logo Alguarisa">
                                             </a>
                                         </div>
                                     </div>
-                                </div>
 
+                                    <form class="position-relative" id="form_login"  novalidate>
+                                        <div class="row gy-3 overflow-hidden">
+                                            <div class="col-12">
+                                                <div class="form-floating mb-2 has-validation">
+                                                    <input id="email" type="email" class="form-control" name="email"  placeholder="name@example.com" required>
+                                                    <label for="email" class="form-label">Correo electrónico</label>
+                                                    <div class="invalid-feedback" id="error_email">
+                                                        Por favor ingrese su Correo electrónico.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="form-floating mb-2 has-validation">
+                                                    <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
+                                                    <label for="password" class="form-label">Contraseña</label>
+                                                    <div class="invalid-feedback" id="error_password">
+                                                        Por favor ingrese su Contraseña.
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12">
+                                                <div class="d-grid">
+                                                    <input type="hidden" name="opcion" value="login">
+                                                    <button class="btn btn-dark btn-lg gradient-custom-2" type="submit">
+                                                        Iniciar sesión
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="position-absolute top-50 start-50 translate-middle d-none verCargando">
+                                            <div class="spinner-border" role="status">
+                                                <span class="visually-hidden">Loading...</span>
+                                            </div>
+                                        </div>
+                                    </form>
+
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-center mt-5">
+                                                <?php if (env('APP_REGISTER', false)){ ?>
+                                                    <a href="../register" class="link-secondary text-decoration-none">
+                                                        Registrarse
+                                                    </a>
+                                                <?php } ?>
+                                                <a href="../forgot-password" class="link-secondary text-decoration-none">
+                                                    ¿Olvidó su contraseña?
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-12 text-center mt-5">
+                                            <small class="link-secondary text-decoration-none">
+                                                &copy; 2024 Dirección de Tecnología y Sistemas.
+                                            </small>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
-                        <div class="col-lg-6 d-none d-lg-flex align-items-center gradient-custom-2" style="min-height: 70vh">
-                            <img class="gobernacion" src="<?php asset('public/img/logo_gobernacion_white.png') ?>" alt="Logo Gobernación Guárico">
-                            <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                                <img class="img-fluid rounded-2 border border-light" src="<?php asset('public/img/logo_tecnologia.png') ?>" alt="Logo Tecnología Alguarisa">
-                            </div>
-                        </div>
+
+
                     </div>
                 </div>
             </div>
         </div>
-
-
     </div>
-</div>
+</section>
 
 <!-- Toast Bootstrap con JS -->
 <div id="toastBootstrap">
